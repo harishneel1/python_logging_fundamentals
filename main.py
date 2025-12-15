3ff8078d638f06291eefec69f80120125fd58864
Harish Neel <harishneel@Harishs-MacBook-Pro.local>
2025-12-15 11:12:26 +0530

    fix

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO, 
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 logging.debug("debug")
 logging.info("info")
